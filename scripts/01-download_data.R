e021288f82fb214ba73d20530e4d7e79e502a665
Quang Mai <quangpono@gmail.com>
2024-03-30 07:48:12 +0000

    download datasets

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -9,18 +9,59 @@
 
 
 #### Workspace setup ####
-library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(tidytext)
+library(rmarkdown)
+library(gutenbergr)
+library(tm)
+library(dplyr)
+library(ggplot2)
 
-#### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+
+# Collect stream-of-conscious authors main texts:
+
+all_books <- gutenberg_metadata
+
+joyce_books <- all_books %>%
+  filter(str_detect(author, 'Joyce, James'))
+
+woolf_books <- all_books %>%
+  filter(str_detect(author, 'Woolf, Virginia'))
+
+dostoevsky_books <- all_books %>%
+  filter(str_detect(author, 'Dostoyevsky, Fyodor'))
+
+proust_books <- all_books %>%
+  filter(str_detect(author, 'Proust, Marcel'))
+
+combined_books <- rbind(joyce_books, woolf_books, dostoevsky_books, proust_books)
 
 
+# Download the text of the most stream-of-consciousness works of each author:
+
+portrait <- gutenberg_download(c('4217')) |>
+  mutate(book = ifelse(gutenberg_id == 4217, "A Portrait of the Artist as a Young Man", "NA")) |>
+  slice(-(1:20))
+
+swann_way <- gutenberg_download(c('7178')) |>
+  mutate(book = ifelse(gutenberg_id == 7178, "Swann's Way", "NA")) |>
+  slice(-(1:33))
+
+mrs_dalloway <- gutenberg_download(c('63107')) |>
+  mutate(book = ifelse(gutenberg_id == 63107, "Mrs Dalloway", "NA")) |>
+  slice(-(1:25))
+
+notes_underground <- gutenberg_download(c('600')) |>
+  mutate(book = ifelse(gutenberg_id == 600, "Notes from the Underground", "NA")) |>
+  slice(-(1:57))
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(portrait, "data/raw_data/portrait.csv") 
+write_csv(swann_way, "data/raw_data/swann_way.csv") 
+write_csv(mrs_dalloway, "data/raw_data/mrs_dalloway.csv") 
+write_csv(notes_underground, "data/raw_data/notes_underground.csv") 
+write_csv(all_books, "data/raw_data/all_books.csv") 
+write_csv(combined_books, "data/raw_data/combined_books.csv") 
 
-         
+
+
--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -13,6 +13,50 @@ library(tidyverse)
 #### Clean data ####
 raw_data <- read_csv("inputs/data/plane_data.csv")
 
+
+tidydataset <- shakes %>%
+  unnest_tokens(word, text)
+
+gutenberg_download(768)
+
+# You can use the tidytext stop_words dataset
+data(stop_words)
+
+# Just use anti_join() to specify the name of the dataset with words that need to be excluded 
+tidydataset <- tidydataset %>%
+  anti_join(stop_words)
+
+# Use count() to quickly get the count of the most frequently used words
+tidydataset %>%
+  count(word, sort = TRUE)
+
+# Convert to Corpus
+corpus <- Corpus(VectorSource(text$text))
+
+# Perform text preprocessing
+corpus_clean <- tm_map(corpus, content_transformer(tolower))
+corpus_clean <- tm_map(corpus_clean, removePunctuation)
+corpus_clean <- tm_map(corpus_clean, removeNumbers)
+corpus_clean <- tm_map(corpus_clean, removeWords, stopwords("english"))
+corpus_clean <- tm_map(corpus_clean, stripWhitespace)
+
+# Convert the corpus to a tidy format
+tidy_corpus <- tidy(corpus_clean)
+
+# Calculate word frequencies
+word_freq <- tidy_corpus %>%
+  count(word, sort = TRUE)
+
+# Visualize word frequencies (e.g., bar plot or word cloud)
+# Example using ggplot2 for bar plot
+top_words <- head(word_freq, 20)
+ggplot(top_words, aes(x = reorder(word, -n), y = n)) +
+  geom_bar(stat = "identity") +
+  labs(x = "Word", y = "Frequency", title = "Top 20 Most Frequent Words")
+
+
+
+
 cleaned_data <-
   raw_data |>
   janitor::clean_names() |>
