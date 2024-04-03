c3a7c4c7ed71b3206b4f9d51037bfd24a30d9f8f
Quang Mai <quangpono@gmail.com>
2024-04-03 07:09:07 +0000

    construct graphs

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,12 +1,10 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the novel texts from Project Gutenberg (https://www.gutenberg.org_
+# Author: Quang Mai
+# Date: 29 March 2024 
+# Contact: quangpono@gmail.com
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Pre-requisites: Install and load packages tidytext and gutenbergr
 
 #### Workspace setup ####
 library(tidyverse)
@@ -29,38 +27,48 @@ joyce_books <- all_books %>%
 woolf_books <- all_books %>%
   filter(str_detect(author, 'Woolf, Virginia'))
 
-dostoevsky_books <- all_books %>%
-  filter(str_detect(author, 'Dostoyevsky, Fyodor'))
+mansfield_books <- all_books %>%
+  filter(str_detect(author, 'Mansfield, Katherine'))
 
 proust_books <- all_books %>%
   filter(str_detect(author, 'Proust, Marcel'))
 
-combined_books <- rbind(joyce_books, woolf_books, dostoevsky_books, proust_books)
+ts_books <- all_books %>%
+  filter(str_detect(author, 'Eliot, T. S.'))
 
 
-# Download the text of the most stream-of-consciousness works of each author:
 
-portrait <- gutenberg_download(c('4217')) |>
-  mutate(book = ifelse(gutenberg_id == 4217, "A Portrait of the Artist as a Young Man", "NA")) |>
+combined_books <- rbind(joyce_books, woolf_books, dostoevsky_books, proust_books, ts_books, mansf)
+
+
+# Download the more famous stream-of-consciousness work of each author:
+
+portrait <- gutenberg_download(c('4217', '2817')) |>
+  mutate(book = ifelse(gutenberg_id == 4217, "A Portrait of the Artist as a Young Man", "Chamber Music")) |>
   slice(-(1:20))
 
 swann_way <- gutenberg_download(c('7178')) |>
   mutate(book = ifelse(gutenberg_id == 7178, "Swann's Way", "NA")) |>
   slice(-(1:33))
 
-mrs_dalloway <- gutenberg_download(c('63107')) |>
-  mutate(book = ifelse(gutenberg_id == 63107, "Mrs Dalloway", "NA")) |>
+mrs_dalloway <- gutenberg_download(c('63107', '5670')) |>
+  mutate(book = ifelse(gutenberg_id == 63107, "Mrs Dalloway", "Jacob's Room")) |>
   slice(-(1:25))
 
-notes_underground <- gutenberg_download(c('600')) |>
-  mutate(book = ifelse(gutenberg_id == 600, "Notes from the Underground", "NA")) |>
-  slice(-(1:57))
+prufrock <- gutenberg_download(c('1321', '1459')) |>
+  mutate(book = ifelse(gutenberg_id == 1321, "The Waste Land", "Prufrock and Other Observations")) |>
+  slice(-(1:35))
+
+bliss <- gutenberg_download(c('44385', '1429')) |>
+  mutate(book = ifelse(gutenberg_id == 1321, "Bliss", "The Garden Party")) |>
+  slice(-(1:34))
 
 #### Save data ####
 write_csv(portrait, "data/raw_data/portrait.csv") 
 write_csv(swann_way, "data/raw_data/swann_way.csv") 
 write_csv(mrs_dalloway, "data/raw_data/mrs_dalloway.csv") 
-write_csv(notes_underground, "data/raw_data/notes_underground.csv") 
+write_csv(prufrock, "data/raw_data/prufrock.csv") 
+write_csv(bliss, "data/raw_data/bliss.csv") 
 write_csv(all_books, "data/raw_data/all_books.csv") 
 write_csv(combined_books, "data/raw_data/combined_books.csv") 
 
--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -4,7 +4,7 @@
 # Date: 30 March 2024 
 # Contact: quangpono@gmail.com
 # License: MIT
-# Pre-requisites: Read four raw datasets containing the four novels
+# Pre-requisites: Read four raw datasets containing the novel texts from five authors
 
 #### Workspace setup ####
 library(tidyverse)
@@ -21,7 +21,9 @@ library(scales)
 portrait <- read_csv("data/raw_data/portrait.csv")
 swann <- read_csv("data/raw_data/swann_way.csv")
 dalloway <- read_csv("data/raw_data/mrs_dalloway.csv")
-notes <- read_csv("data/raw_data/notes_underground.csv")
+bliss <- read_csv("data/raw_data/bliss.csv")
+prufrock <- read_csv("data/raw_data/prufrock.csv")
+
 
 # Tokenize the words from the text, breaking the text down into individual words to insert them into individual datasets
 # Codes sourced from https://medium.com/the-data-nudge/nlp-basics-exploring-word-frequency-with-the-tidytext-r-package-ac35a6d805f4
@@ -38,24 +40,36 @@ dalloway_tokenized <- dalloway |>
   na.omit(dalloway) |>
   unnest_tokens(word, text)
 
-notes_tokenized <- notes |>
-  na.omit(notes) |>
+prufrock_tokenized <- prufrock |>
+  na.omit(prufrock) |>
+  unnest_tokens(word, text)
+
+bliss_tokenized <- bliss |>
+  na.omit(bliss) |>
   unnest_tokens(word, text)
 
 # Exclude stop word using the tidytext stop_words dataset, also, use anti_join() to specify the name of the dataset with words that need to be excluded
 data(stop_words)
 
 portrait_clean <- portrait_tokenized |>
-  anti_join(stop_words)
+  anti_join(stop_words) |>
+  filter(nchar(word) > 1)
 
 swann_clean <- swann_tokenized |>
-  anti_join(stop_words)
+  anti_join(stop_words) |>
+  filter(nchar(word) > 1)
 
 dalloway_clean <- dalloway_tokenized |>
-  anti_join(stop_words)
+  anti_join(stop_words) |>
+  filter(nchar(word) > 1)
 
-notes_clean <- notes_tokenized |>
-  anti_join(stop_words)
+bliss_clean <- bliss_tokenized |>
+  anti_join(stop_words) |>
+  filter(nchar(word) > 1)
+
+prufrock_clean <- prufrock_tokenized |>
+  anti_join(stop_words) |>
+  filter(nchar(word) > 1)
 
 # Use count() to quickly get the count of the most frequently used words
 portrait_words <- portrait_clean |>
@@ -67,12 +81,17 @@ swann_words <- swann_clean |>
 dalloway_words <- dalloway_clean |>
   count(word, sort = TRUE)
 
-notes_words <- notes_clean |>
+bliss_words <- bliss_clean |>
   count(word, sort = TRUE)
 
+prufrock_words <- prufrock_clean |>
+  count(word, sort = TRUE)
 
 #### Save data ####
+write_csv(portrait_clean, "data/analysis_data/portrait_clean.csv")
 write_csv(portrait_words, "data/analysis_data/portrait_words.csv")
 write_csv(swann_words, "data/analysis_data/swann_words.csv")
 write_csv(dalloway_words, "data/analysis_data/dalloway_words.csv")
-write_csv(notes_words, "data/analysis_data/notes_words.csv")
+write_csv(bliss_words, "data/analysis_data/bliss_words.csv")
+write_csv(prufrock_words, "data/analysis_data/prufrock_words.csv")
+
