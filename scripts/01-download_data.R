7e9471cc42106ebd2a7415ebaacd6e810734e685
ponolite <147739831+ponolite@users.noreply.github.com>
2024-04-03 21:37:01 -0400

    Update 01-download_data.R

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -17,7 +17,7 @@ library(ggplot2)
 
 
 # Collect stream-of-conscious authors main texts
-# Codes sourced from https://medium.com/the-data-nudge/nlp-basics-exploring-word-frequency-with-the-tidytext-r-package-ac35a6d805f4
+# Codes referenced from https://medium.com/the-data-nudge/nlp-basics-exploring-word-frequency-with-the-tidytext-r-package-ac35a6d805f4
 
 all_books <- gutenberg_metadata
 
